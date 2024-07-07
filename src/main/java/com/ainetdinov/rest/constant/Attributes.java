6b76562a88cd4a19138e937cad48fe9d1f4b44ed
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-07 11:09:09 +0300

    Добавил константы

---
+++ src/main/java/com/ainetdinov/rest/constant/Attributes.java
@@ -0,0 +1,5 @@
+package com.ainetdinov.rest.constant;
+
+public class Attributes {
+    public static final String STUDENT_SERVICE = "studentService";
+}
---
+++ src/main/java/com/ainetdinov/rest/constant/Endpoint.java
@@ -0,0 +1,11 @@
+package com.ainetdinov.rest.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Endpoint {
+    public static final String TEACHER = "teacher";
+    public static final String STUDENTS = "students";
+    public static final String SLASH = "/";
+    public static final String ASTERISK = "*";
+}
