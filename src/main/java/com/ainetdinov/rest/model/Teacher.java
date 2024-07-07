c46cccf629e574b9de7fad8e89948d17465604a4
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-07 13:54:50 +0300

    Добавил toString()

--- src/main/java/com/ainetdinov/rest/model/Teacher.java
+++ src/main/java/com/ainetdinov/rest/model/Teacher.java
@@ -1,18 +1,32 @@
 package com.ainetdinov.rest.model;
 
 import com.ainetdinov.rest.constant.Subject;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.util.List;
 
 @Data
 @Builder
 @Jacksonized
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
 public class Teacher {
     private Long id;
     private String name;
     private int experience;
     private List<Subject> subjects;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append("id", id)
+                .append("name", name)
+                .append("experience", experience)
+                .append("subjects", subjects)
+                .toString();
+    }
 }
