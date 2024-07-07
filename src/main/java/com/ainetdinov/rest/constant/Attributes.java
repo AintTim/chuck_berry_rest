29b0dd09cb2be42586624216f75a1037fbf62dc9
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-07 13:54:11 +0300

    Добавил аттрибуты

--- src/main/java/com/ainetdinov/rest/constant/Attributes.java
+++ src/main/java/com/ainetdinov/rest/constant/Attributes.java
@@ -2,4 +2,7 @@ package com.ainetdinov.rest.constant;
 
 public class Attributes {
     public static final String STUDENT_SERVICE = "studentService";
+    public static final String TEACHER_SERVICE = "teacherService";
+    public static final String GROUP_SERVICE = "groupService";
+    public static final String SCHEDULE_SERVICE = "scheduleService";
 }
