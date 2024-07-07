bbe6b8097cc33a8a4567979502b5cd6088cfc351
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-07 16:36:03 +0300

    Добавил HttpService

---
+++ src/main/java/com/ainetdinov/rest/service/HttpService.java
@@ -0,0 +1,32 @@
+package com.ainetdinov.rest.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Objects;
+
+import static com.ainetdinov.rest.constant.Endpoint.SLASH;
+
+public class HttpService {
+    private final String EMPTY_STRING = "";
+
+    public int extractId(HttpServletRequest request) {
+        return Integer.parseInt(request.getPathInfo().replace(SLASH, EMPTY_STRING));
+    }
+
+    public boolean containsQueryString(HttpServletRequest request) {
+        String queryString = request.getQueryString();
+        return Objects.nonNull(queryString) && !queryString.isEmpty();
+    }
+
+    public boolean containsPath(HttpServletRequest request) {
+        String path = request.getPathInfo();
+        return Objects.nonNull(path) && !path.replace(SLASH, EMPTY_STRING).isEmpty();
+    }
+
+    public void prepareResponse(HttpServletResponse response) {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-Type", "application/json");
+    }
+}
