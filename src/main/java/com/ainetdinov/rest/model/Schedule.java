d656603b3cf5ec6dd7b179ccf69c630bd30e2756
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-07-07 12:16:32 +0300

    Обновил формат данных

--- src/main/java/com/ainetdinov/rest/model/Schedule.java
+++ src/main/java/com/ainetdinov/rest/model/Schedule.java
@@ -5,16 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @Jacksonized
 public class Schedule {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:MM")
-    private LocalDate start;
+    private LocalDateTime start;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:MM")
-    private LocalDate end;
+    private LocalDateTime end;
     private Long groupId;
     private Long teacherId;
 }
